63b0da998078c34a04d6c512c2989ccaa551802f
Chad Wollenberg <chad.wollenberg@me.com>
2016-02-03 00:08:30 -0600

    completed exercise, still some rough edges to smooth out. especially in UI

---
+++ oopExcersize/Character.swift
@@ -0,0 +1,54 @@
+//
+//  Character.swift
+//  oopExcersize
+//
+//  Created by Chad Wollenberg on 2/2/16.
+//  Copyright © 2016 Chad Wollenberg. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 12
+    private var _name:String = "Player"
+    
+    var hp:Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var name:String {
+        get {
+            return _name
+        }
+    }
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+               return true
+            }
+        }
+    }
+    
+    init (startingHp: Int, startingAttackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = startingAttackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        
+        return true
+    }
+}
---
+++ oopExcersize/Player1.swift
@@ -0,0 +1,11 @@
+//
+//  Player1.swift
+//  oopExcersize
+//
+//  Created by Chad Wollenberg on 2/2/16.
+//  Copyright © 2016 Chad Wollenberg. All rights reserved.
+//
+
+import Foundation
+
+
