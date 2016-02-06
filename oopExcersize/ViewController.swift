66cf80dc4296f0c95ba92035ec1aa3034f6aa49e
Chad Wollenberg <chad.wollenberg@me.com>
2016-02-06 15:51:27 -0600

    fixed name submission labels and cleaned up some repeated code

--- oopExcersize/ViewController.swift
+++ oopExcersize/ViewController.swift
@@ -14,10 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var Enemy: UIImageView!
     @IBOutlet weak var Hero: UIImageView!
     @IBOutlet weak var statLbl: UILabel!
-     @IBOutlet weak var enemyNameField: UITextField!
+    @IBOutlet weak var enemyNameField: UITextField!
     @IBOutlet weak var doneBtnLbl: UILabel!
     @IBOutlet weak var heroNameField: UITextField!
     
+
     @IBOutlet weak var heroNameBtn: UIButton!
     @IBOutlet weak var EnemyNameBtn: UIButton!
     var hero: Character!
@@ -26,47 +27,56 @@ class ViewController: UIViewController {
     
    
     @IBAction func onEnemySubmitNamePressed(sender: AnyObject) {
+        setname(enemy, playerNameField: enemyNameField, playerNameBtn: EnemyNameBtn)
+        
     }
     
     @IBAction func onHeroSubmitNamePressed(sender: UIButton) {
-        heroNameBtn.hidden = true
-        heroNameBtn.hidden = true
-        doneBtnLbl.hidden = true
-        heroNameField.hidden = true
-        heroNameField.text = ""
+        setname(hero, playerNameField: heroNameField, playerNameBtn: heroNameBtn)
     }
     
     @IBAction func OnHeroAttackPressed(sender: AnyObject) {
-        
-            Attack(hero, defender: enemy)
-    
-    
+        showNameinfo(heroNameBtn)
     }
     
    
     @IBAction func onEnemyAttackPressed(sender: AnyObject) {
+        if enemy.name == "" {
         
-            Attack(enemy, defender: hero)
-        
+        } else {
+          Attack(enemy, defender: hero)
+        }
     }
+    
+    
     func Attack(attacker: Character, defender: Character) {
         defender.defendAttack(attacker.attackPwr)
         statLbl.text = "\(attacker.name) has hit \(defender.name) for \(attacker.attackPwr) HP! \n \(defender.name)'s hp is now \(defender.hp)"
     }
     
-    func setName(player: Character) {
+    func showNameinfo (playerBtn: UIButton) {
         doneBtnLbl.hidden = false
-        heroNameBtn.hidden = false
-        heroNameField.hidden = false
+        playerBtn.hidden = false
+    }
+    
+    func setname (player: Character, playerNameField: UITextField, playerNameBtn: UIButton) {
+        playerNameBtn.hidden = true
+        doneBtnLbl.hidden = true
+        playerNameField.hidden = true
+        if playerNameField.text! != "" {
+         player.name = playerNameField.text!
+        }
+        
         
     }
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        hero = Character(name: "Player 2", Hp: 100, AttackPwr: 13)
-        enemy = Character(name: "Player 1", Hp: 100, AttackPwr: 13)
+        hero = Character(name: "", Hp: 100, AttackPwr: 13)
+        enemy = Character(name: "", Hp: 100, AttackPwr: 13)
      
     }
 
